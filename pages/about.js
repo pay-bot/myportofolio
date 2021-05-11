f0ddbc2af3188d10c2064cebbe91e84ee2c9207a
unknown <afahri22@yahoo.com>
2021-05-11 09:13:39 -0700

    tes

--- pages/about.js
+++ pages/about.js
@@ -6,15 +6,17 @@ import Index from '../components/About/Index'
 
 const about = () => {
   return (
-    <Wrapper>
-      <Head>
-        <meta property="og:url" content={SEO_DATA.url} key="ogurl" />
-        <meta property="og:image" content={SEO_DATA.link_image} key="ogimage" />
-        <meta property="og:title" content={'About'} key="ogtitle" />
-        <title>{SEO_DATA.title + ' - About'}</title>
-      </Head>
-      <Index />
-    </Wrapper>
+    <>
+      <Wrapper>
+        <Head>
+          <meta property="og:url" content={SEO_DATA.url} key="ogurl" />
+          <meta property="og:image" content={SEO_DATA.link_image} key="ogimage" />
+          <meta property="og:title" content={'About'} key="ogtitle" />
+          <title>{SEO_DATA.title + ' - About'}</title>
+        </Head>
+        <Index />
+      </Wrapper>
+    </>
   )
 }
 
