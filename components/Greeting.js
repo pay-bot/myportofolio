72f2fa0e5c251ce3ce85a637d9f5a556db581d83
unknown <afahri22@yahoo.com>
2021-05-10 18:38:19 -0700

    fix

--- components/Greeting.js
+++ components/Greeting.js
@@ -7,7 +7,7 @@ export default function Greeting() {
       <div className="text-4xl sm:text-5xl md:text-6xl font-black  dark:text-gray-300">Saya <span className="text-blue-500">Fahri</span>
       </div>
 
-      <div className="mt-5 mb-5 sm:text-xl font-semibold  text-gray-400">"Di saat VScode kubuka di situlah aplikasimu akan jadi nyata"</div>
+      <div className="mt-5 mb-5 sm:text-xl font-semibold  text-gray-400">"Divv saat VScode kubuka di situlah aplikasimu akan jadi nyata"</div>
 
 
       <div className="flex lg:justify-start justify-center">
