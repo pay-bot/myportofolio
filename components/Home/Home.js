b69d39e275ab6a98045abc796af0697acb6b760c
unknown <afahri22@yahoo.com>
2021-07-12 22:15:20 +0700

    framer 99%

--- components/Home/Home.js
+++ components/Home/Home.js
@@ -1,52 +1,20 @@
-import CarouselItem from "./CarouselItem"
 import Greeting from "./Greeting"
-import ProfileCard from "./ProfileCard";
-import SkillsTitle from "./SkillsTitle";
 import Layanan from "./Layanan";
-import SertifikatItem from "./SertifikatItem";
-import { motion, useAnimation } from "framer-motion"
+import { motion } from "framer-motion"
 import Citation from "./Citation";
 import React, { useEffect, useState } from "react";
-import { CustomCard } from '@tsamantanis/react-glassmorphism'
 import '@tsamantanis/react-glassmorphism/dist/index.css'
-import { useInView } from 'react-intersection-observer';
+import SkillsCert from "./SkillsCert";
 
 export default function Home() {
-  
+
   const [citation, setCitation] = useState(true);
-  
   useEffect(() => {
     const ids = [
       setTimeout(() => setCitation(false), 4800),
     ];
-    
     return () => ids.forEach((id) => clearTimeout(id));
   }, [setCitation]);
-
-  const { ref, inView} = useInView({
-    threshold: 0,
-  });
-  const animation = useAnimation();
-
-  useEffect(() => {
-
-    if (inView) {
-      animation.start({
-        y: 0,
-        transition: {
-          y: {duration: 2,stiffness: 1000, velocity: -100 }
-        
-        }
-      });
-    }
-    if (!inView) {
-      animation.start({
-        y: 50,
-        transition: {
-          y: { stiffness: 1000 }}})
-    }
-  },
-    [inView]);
 
   return (
     <>
@@ -67,25 +35,13 @@ export default function Home() {
             <img src="expressjs.svg" alt="" className="right-8 flex absolute animate-wiggle lg:w-20 w-16 h-16 lg:h-20 w-16 h-16 rounded-full p-2 border glassmorphism-25 hover:bg-gray-400 z-10" />
             <img src="react-2.svg" alt="" className="lg:-ml-14 -ml-9 flex absolute animate-wiggle lg:w-20 w-16 h-16 lg:h-20 w-16 h-16 rounded-full p-2 mt-52 border glassmorphism-25 hover:bg-gray-400" />
             <img src="node-js.svg" alt="" className="lg:-right-8 -right-3 flex absolute animate-wiggle lg:w-20 w-16 h-16 lg:h-20 w-16 h-16 rounded-full p-2 mt-52 border glassmorphism-25 hover:bg-gray-400" />
-            <img src="fix porto.png" alt="" className=" absolute lg:h-[500px] h-[415px] " />
+            <img src="porto.png" alt="" className=" absolute lg:h-[500px] h-[415px] " />
           </div>
         </motion.div>
       </div>
-      <div ref={ref} className="mt-2 bg-gradient-conic-t from-gray-900 via-gray-100 to-gray-900 dark:bg-gradient-to-r dark:from-blue-gray-800 dark:via-blue-800 dark:to-blue-gray-800 ">
-        <motion.div className="mb-10 md:p-5 p-0 "
-          animate={animation}>
+     
           <Layanan />
-        </motion.div>
-      </div>
-      <div className="flex flex-col w-9/12 mx-auto lg:p-5 p-0">
-        <SkillsTitle>Keterampilan saya</SkillsTitle>
-        <CarouselItem />
-      </div>
-      <div className="lg:w-9/12 w-11/12 mx-auto mb-10 p-5">
-        <p className="mt-0 mb-5 font-semibbold md:text-4xl text-3xl dark:text-gray-200 text-center">Sertifikat</p>
-        <SertifikatItem />
-      </div>
-
+          <SkillsCert />
     </>
   )
 }
--- components/Home/Layanan.js
+++ components/Home/Layanan.js
@@ -1,11 +1,47 @@
 import LayananCard from "./LayananCard"
 import LayananCardTitle from "./LayananCardTitle"
 import LayananTitle from "./LayananTitle"
+import { useInView } from 'react-intersection-observer';
+import { motion, useAnimation } from "framer-motion"
+import React, { useEffect } from "react";
+
+
+
 
 export default function Layanan() {
 
+  const { ref, inView } = useInView({
+    threshold: 0,
+  });
+  const animation = useAnimation();
+
+  useEffect(() => {
+
+    if (inView) {
+      animation.start({
+        y: 0,
+        transition: {
+          y: { duration: 2, stiffness: 1000, velocity: -100, type: 'spring', bounce: 0.3 }
+
+        }
+      });
+    }
+    if (!inView) {
+      animation.start({
+        y: 50,
+        transition: {
+          y: { stiffness: 1000 }
+        }
+      })
+    }
+  },
+    [inView]);
+
   return (
     <>
+    <div ref={ref} className="mt-2 bg-gradient-conic-t from-gray-900 via-gray-100 to-gray-900 dark:bg-gradient-to-r dark:from-blue-gray-800 dark:via-blue-800 dark:to-blue-gray-800 ">
+    <motion.div className="mb-10 md:p-5 p-0 "
+      animate={animation}>
       <div className="lg:w-9/12 w-11/12 mx-auto py-10">
         <LayananTitle>Layanan terbaik saya</LayananTitle>
         <div className="p-5">
@@ -41,6 +77,8 @@ export default function Layanan() {
             />
           </div>
         </div>
+      </div>
+      </motion.div>
       </div>
     </>
   )
