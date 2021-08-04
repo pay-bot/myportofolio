fd2ae987bf28ec5caa6bd3832bacc8169f00fc03
unknown <afahri22@yahoo.com>
2021-08-04 12:40:21 +0700

    refactor

--- components/Home/Hero.js
+++ components/Home/Hero.js
@@ -42,12 +42,12 @@ export default function Hero() {
           initial={{ opacity: 0 }}
           animate={{ opacity: 1 }}
           transition={{ duration: 2, delay: 5 }}>
-          <div className="rounded-full mx-auto relative lg:h-[400px] lg:w-[400px] h-[330px] w-[330px] px-5 border-4 border-green-500 hover:border-green-400 ">
+          <div className="rounded-full mx-auto relative lg:h-[400px] lg:w-[400px] h-[330px] w-[330px] px-5 border-4 border-green-500 hover:border-green-400  ">
             <img src="mongodb.svg" alt="" className="ml-4 flex absolute animate-wiggle lg:w-20 w-16 h-16 lg:h-20 w-16 h-16 rounded-full p-2 border glassmorphism-25 hover:bg-gray-400 z-10" />
             <img src="expressjs.svg" alt="" className="right-8 flex absolute animate-wiggle lg:w-20 w-16 h-16 lg:h-20 w-16 h-16 rounded-full p-2 border glassmorphism-25 hover:bg-gray-400 z-10" />
             <img src="react-2.svg" alt="" className="lg:-ml-14 -ml-9 flex absolute animate-wiggle lg:w-20 w-16 h-16 lg:h-20 w-16 h-16 rounded-full p-2 mt-52 border glassmorphism-25 hover:bg-gray-400" />
             <img src="node-js.svg" alt="" className="lg:-right-8 -right-3 flex absolute animate-wiggle lg:w-20 w-16 h-16 lg:h-20 w-16 h-16 rounded-full p-2 mt-52 border glassmorphism-25 hover:bg-gray-400" />
-            <img src="porto.png" alt="" height="550" className=" absolute lg:h-[500px] h-[415px] " />
+            <img src="porto.png" alt="" height="550" className=" absolute lg:h-[500px] h-[415px]" />
           </div>
         </motion.div>
       </div>
--- components/Project/ProjectCard.js
+++ components/Project/ProjectCard.js
@@ -5,11 +5,11 @@ export default function ProjectCard({ href, image, judul, deskripsi, deskripsiCa
   return (
     <>
       <div className={clsx("flex md:mb-20 mb-10", rightShift && "flex-row-reverse")}>
-        <div className={clsx("z-30 hover:md:z-0 hover:-z-10 md:w-7/12 md:relative absolute md:mr-0 mr-5 ", rightShift && "md:ml-0 md:-mr-0 -mr-0 ml-5"
+        <div className={clsx("z-30 hover:md:z-0 hover:-z-10 md:w-7/12 w-11/12 md:relative absolute  ", rightShift && ""
         )}>
-          <img src={image} alt="" className=" rounded shadow-2xl md:h-[400px] h-[266px] " />
+          <img src={image} alt="" className=" transform transition duration-500 ease-in-out md:hover:scale-110  rounded shadow-2xl md:h-[400px] h-[276px] " />
         </div>
-        <div className={clsx("select-none md:w-5/12 w-full  md:z-30 z-0 hover:z-30 relative md:mt-10 mt-0 md:px-0 px-3 md:py-0 pt-2  dark:md:bg-transparent dark:bg-gray-700 dark:bg-opacity-90 bg-white bg-opacity-80 md:bg-opacity-0 rounded ", rightShift && "flex-row-reverse ")}
+        <div className={clsx("md:opacity-100 opacity-0 transform transition duration-1000 ease-in-out hover:opacity-100 select-none md:w-5/12 w-full md:z-30 z-0 hover:z-30 relative md:mt-10 mt-0 md:px-0 px-3 md:py-0 pt-2  dark:md:bg-transparent dark:bg-gray-700 dark:bg-opacity-90 bg-white bg-opacity-80 md:bg-opacity-0 rounded ", rightShift && "flex-row-reverse ")}
         >
           <p className={clsx("capitalize text-right text-xl font-semibold dark:md:text-gray-200 md:text-gray-700 text-black dark:text-gray-200 ", rightShift && "md:text-left text-right flex ")}>{judul}</p>
           <p className={clsx("text-right capitalize md:text-md text-lg font-semibold dark:md:text-gray-200 md:text-gray-700 text-black dark:text-gray-200 ", rightShift && "md:text-left text-right flex ")}>{deskripsi}</p>
@@ -18,12 +18,18 @@ export default function ProjectCard({ href, image, judul, deskripsi, deskripsiCa
               {deskripsiCard}
             </p>
           </div>
-          <div className={clsx("md:mt-20 mt-40 bottom-0 md:bottom-5 flex justify-start right-0 mb-3 flex-row-reverse md:absolute relative ", rightShift && "flex  flex-row md:left-0 right-0 justify-start")}>
+          <div className={clsx("md:mt-20 mt-40 bottom-0 md:bottom-5 flex justify-start right-0 mb-5 flex-row-reverse md:absolute relative ", rightShift && "flex  flex-row md:left-0 right-0 justify-start")}>
             {techImage.map((tech, i) =>
-              <img key={tech} src={tech} className="w-8 h-8 mx-2" alt="" />
+              <img key={tech} src={tech} className="w-8 h-8 mx-2 transform transition duration-500 ease-in-out hover:scale-125" alt="" />
 
             )}
-            <div className=""><Link href={href}><a className="relative "><img src="/link.svg" alt="" className="w-8 h-8 mx-2" /></a></Link>
+            <div className="relative flex"><Link href={href}>
+              <a className="relative transform transition duration-500 ease-in-out hover:scale-125">
+                <img src="/link.svg" alt="" className="w-8 h-8 mx-2 " />
+            <div className="h-full inset-0 pb-10 justify-center w-full absolute flex opacity-0 transform transition duration-1000 ease-in-out hover:opacity-100 ">
+              <p className="bg-white mt-7 font-bold text-center tracking-wide capitalize">go</p>
+              </div>
+            </a></Link>
             </div>
           </div>
         </div>
