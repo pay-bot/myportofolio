d962c7228d35582f9887653b10e7796efba394f7
unknown <afahri22@yahoo.com>
2021-09-03 10:06:39 +0700

    change to asc

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -9,11 +9,11 @@ export default function page({ detailPost }) {
   const [detail, setDetail] = useState(detailPost);
   const router = useRouter();
   const { locale } = router;
-  const  language = locale;
+  const language = locale;
   return (
     <>
       <div className="md:w-9/12 w-full mx-auto md:px-6 px-2">
-      <img src={detail.img_url} alt="" className="md:w-screen md:h-[600px] mt-20 object-cover" />
+        <img src={detail.img_url} alt="" className="md:w-screen md:h-[600px] mt-20" />
         <div className="my-10">
           <h1 className="text-5xl text-center text-gray-700 dark:text-gray-50">{language === 'en' ? detail.title : detail.title_idn}</h1>
           <div className="flex justify-center my-5">
