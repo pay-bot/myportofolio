4f840494fecf64e8568d445dac9444e1a76d475e
unknown <afahri22@yahoo.com>
2021-09-08 10:18:14 +0700

    axios post

--- utils/api.js
+++ utils/api.js
@@ -1,51 +1,51 @@
 import axios from "axios";
 
 export const postLike = async (slug) => {
-    const res = await axios.post(`/like/slug=${slug}`, {
-  headers: {
-    'content-type': 'application/json'
-  },
-});
-return res.json();
-}
+  const res = await axios.post('cmsku.herokuapp.com/api/project/like/slug?=dipoles', {
+    headers: {
+      'content-type': 'application/json'
+    },
+  });
+  return res.json();
 
 
-export const getDetailPost = async (slug) => {
-  try {
-    const data = await axios.get(
-      `${process.env.API_URL}/blog?slug=${slug}`
-    );
-    return data.data[0];
-  } catch (error) {
-    return [];
-  }
-};
 
-export const getPost = async () => {
-  try {
-    const data = await axios.get(`${process.env.API_URL}/blog?_sort=id:desc`)
-    return data.data
-  } catch (error) {
-    return [];
-  }
-};
+  export const getDetailPost = async (slug) => {
+    try {
+      const data = await axios.get(
+        `${process.env.API_URL}/blog?slug=${slug}`
+      );
+      return data.data[0];
+    } catch (error) {
+      return [];
+    }
+  };
 
-export const getProject = async () => {
-  try {
-    const data = await axios.get(`${process.env.API_URL}/project?_sort=id:desc`)
-    return data.data
-  } catch (error) {
-    return [];
-  }
-};
+  export const getPost = async () => {
+    try {
+      const data = await axios.get(`${process.env.API_URL}/blog?_sort=id:desc`)
+      return data.data
+    } catch (error) {
+      return [];
+    }
+  };
 
-export const getProjectByCategory = async (id) => {
-  try {
-    const data = await axios.get(
-      `${process.env.API_URL}/project/categories?category_id=${id}`
-    );
-    return data.data[0];
-  } catch (error) {
-    return [];
-  }
-};
+  export const getProject = async () => {
+    try {
+      const data = await axios.get(`${process.env.API_URL}/project?_sort=id:desc`)
+      return data.data
+    } catch (error) {
+      return [];
+    }
+  };
+
+  export const getProjectByCategory = async (id) => {
+    try {
+      const data = await axios.get(
+        `${process.env.API_URL}/project/categories?category_id=${id}`
+      );
+      return data.data[0];
+    } catch (error) {
+      return [];
+    }
+  };
