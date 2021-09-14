838d789ab17e31f3166c06db9af4e3bbb977d60d
unknown <afahri22@yahoo.com>
2021-09-14 09:48:59 +0700

    rf

--- components/Skills/SkillsItem.js
+++ components/Skills/SkillsItem.js
@@ -77,7 +77,7 @@ export default function SkillsItem() {
       {skills.map((data, i) => (
       <div data-aos="fade-up" data-aos-offset={interval * i} className='transform transition duration-500 ease-in-out hover:scale-110 dark:bg-gradient-to-t dark:from-blue-600 dark:via-gray-700 dark:to-gray-900 border-gray-600 dark:border-gray-600 bg-gradient-to-t from-gray-100 via-gray-200 to-[#EAECF4] p-6 flex flex-col items-center m-2 justify-center' key ={i}>
         <div className='h-full'>
-          <img className='w-24' src={data.image} alt={data.alt} />
+          <img className="" width="96px" height="96px" src={data.image} alt={data.alt} />
         </div>
         <div className='flex flex-col items-center'>
           <span className='block text-xl mb-2 dark:text-white'>{data.title}</span>
